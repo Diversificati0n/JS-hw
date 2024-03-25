bfb6a213e38a42140331e82270a92c1e5f874eda
Diversification <startsevprm@gmail.com>
2024-03-25 15:40:36 +0500

    first commit

---
+++ hw1/script.js
@@ -0,0 +1,13 @@
+
+    // Задание 1
+    let celsiusTemperature = 25;
+    let fahrenheitTemperature = (9 / 5) * celsiusTemperature + 32;
+
+    console.log(celsiusTemperature);
+    console.log(fahrenheitTemperature);
+
+    // Задание 2
+    let name = "Andrey";
+    let admin = name;
+
+    console.log(admin);
